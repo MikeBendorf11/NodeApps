bc8132042dba3510847650e4f962179098f5cd4b
MikeB <mikebendorf11@gmail.com>
2018-11-19 20:13:06 -0800

    Customer info, confirmation page, calculating delivery time and taxes, saving orders as jason with time

--- index.js
+++ index.js
@@ -4,30 +4,70 @@ const app = express();
 const bodyParser = require('body-parser');
 const menu = require('./pizza-menu')
 const Order = require('./order')
-
+const validator = require('validator');
+const fs = require('fs');
+let order = {}
 
 //console.log(menu);
 //app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.urlencoded({extended:false}));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-	res.render('index', { menu: menu});
+	res.render('index', { menu: menu, errors: {}});
 });
 app.post('/confirmation/', (req,res)=>{
   //console.log(req);
   const body =  req.body; 
   const toppings = []; 
   
-  console.log(body);
+  //console.log(body);
   for (var key in body){
    // console.log(body[key]);
     if(body[key]=='on'){
     toppings.push(key);
     }
   }
-  let order = new Order(toppings, body.crust, body.size);
-  console.log(order.getTotals());
-  res.render('confirmation',{ order: order }) 
+  let inputErrors = {};
+  var flag = false;
+  inputErrors.name = !validator.isAlpha(body.name.replace(/\s+/, "")) ? "Invalid name" : "";
+  inputErrors.phone = !validator.isMobilePhone(body.phone) ? "Invalid phone": "";
+  inputErrors.address = body.address.length < 8 ? "Invalid address":"";
+  for(var key in inputErrors){
+    if(inputErrors[key]){
+      flag = true;
+      //console.log(inputErrors[key]);
+    }
+  }
+  if(flag){
+    order = {}
+    res.render('index', {menu: menu, errors: inputErrors} );
+  } else {
+    order = new Order(
+      toppings, 
+      body.crust, 
+      body.size,
+      body.name,
+      body.phone,
+      body.address,
+      body.quantity
+    );
+    res.render('confirmation',{ order: order, totals: order.getTotals() }) 
+  }
+})
+
+app.post('/index/',(req, res)=>{
+  //console.log(req.body.confirm);
+  const body =  req.body; 
+  if(body.confirm=='yes'){
+    let json = JSON.stringify(order, null, '\t');
+    fs.writeFile('./orders/'+Date.now()+'.json',json, (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    })
+  } else {
+    order = {};
+  }
+  res.render('index', { menu: menu, errors: {} });
 })
 
 var server = app.listen(3000, ()=>{
--- order.js
+++ order.js
@@ -2,10 +2,14 @@ const menu = require('./pizza-menu')
 const tax = 1.12;
 
 module.exports = class Order{
-  constructor(toppings, crust, size){
+  constructor(toppings, crust, size, name, phone, address, quantity){
     this.toppings = toppings;
     this.crust = crust;
     this.size = size;
+    this.name = name;
+    this.phone = phone;
+    this.address = address;
+    this.quantity = quantity;
   }
   getTotals(){
     let base = 0;
@@ -16,9 +20,22 @@ module.exports = class Order{
       }
     });
     base += menu.crust[this.crust]
-    var beforeTax = base * factor;
+    var beforeTax = base * factor * this.quantity;
     var afterTax = beforeTax * tax;
-    return {beforeTax: beforeTax, afterTax: afterTax};
+    return {
+      tax: afterTax - beforeTax,
+      beforeTax: beforeTax, 
+      afterTax: afterTax
+    };
   }
-  
+  getDelivery(){
+    let time = 0;
+    if(this.quantity>1){
+      time = this.quantity * 15;
+      let hours = time/60;
+      let minutes = (hours%1)*60;
+      return Math.trunc(hours) + 'h:' + minutes + 'm';
+    } 
+    return '30m'
+  }
 }
